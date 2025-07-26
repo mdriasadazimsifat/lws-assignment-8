73795ad661c701d45a11be2f00f2b8c21718e02e
ras <rasifat503@gmail.com>
2025-07-26 23:39:43 +0600

    some change

--- app/utils/getMessages.js
+++ app/utils/getMessages.js
@@ -1,14 +1,23 @@
+import connectMongo from "../lib/dbConnect/connectMongo";
+import { Conversation } from "../lib/models/Conversation";
+import Message from "../lib/models/Message";
+import replaceArrayId from "./replaceArrayId";
+
 export default async function getMessages(conversationId) {
   try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/conversation/${conversationId}`,
-      { cache: "no-store" }
-    );
-    if (!response.ok) {
-      return { messages: [], title: "" };
-    }
+    await connectMongo();
+    const messages = await Message.find({ sessionId: conversationId }).lean();
+    const conversation = await Conversation.findById(conversationId);
+    const replaceIdInArray = replaceArrayId(messages);
+    // const response = await fetch(
+    //   `${process.env.NEXT_PUBLIC_BASE_URL}/api/conversation/${conversationId}`,
+    //   { cache: "no-store" }
+    // );
+    // if (!response.ok) {
+    //   return { messages: [], title: "" };
+    // }
 
-    const result = await response.json();
+    const result = { messages, title: conversation.title };
 
     return result;
   } catch (e) {
